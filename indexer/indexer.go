c6ed8078b7f12e8ed479ae311e1327b5c16b5310
Javier Torres <javierandrestorresreyes@gmail.com>
2023-01-12 16:02:53 -0500

    basic indexer

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"net/mail"
 	"os"
 	"path/filepath"
@@ -66,7 +69,7 @@ func emailMessages(maildir string) ([]*EmailJson, error) {
 	if err != nil {
 		return nil, err
 	}
-	paths = paths[:100000]
+	fmt.Println(len(paths), "being parsed")
 	var messages []*EmailJson
 	for _, path := range paths {
 		msg, err := parseEmail(path)
@@ -83,15 +86,56 @@ func emailMessages(maildir string) ([]*EmailJson, error) {
 	return messages, nil
 }
 
+func uploadEmails(maildir string) error {
+	emails, err := emailMessages(maildir)
+	if err != nil {
+		return err
+	}
+
+	// Create the payload in the correct format
+	payload := map[string]interface{}{
+		"index":   "email",
+		"records": emails,
+	}
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	// Send the POST request
+	url := "http://localhost:4080/api/_bulkv2"
+	req, err := http.NewRequest("POST", url, bytes.NewReader(payloadBytes))
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.SetBasicAuth("admin", "Complexpass#123")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Print the response status and body
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	responseBody, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(responseBody))
+
+	return nil
+}
+
 func main() {
 	fmt.Println("Starting...")
 	start := time.Now()
-	msgs, err := emailMessages("../enron_mail_20110402/maildir")
+	maildir := "../enron_mail_20110402/maildir"
+	err := uploadEmails(maildir)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error uploading emails: ", err)
 		return
 	}
-	fmt.Println(len(msgs), "emails found and parsed")
+	fmt.Println("Emails uploaded successfully")
 	elapsed := time.Since(start)
 	fmt.Printf("Time taken: %s\n", elapsed)
 }
